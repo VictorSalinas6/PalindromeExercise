c29d50d44e3b8c7b520a2ce864bd2ed04d0ae950
Victor Salinas <victorsalinas6@gmail.com>
2024-01-02 23:09:05 -0600

    Finished the tests

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+
+            var charArray = word.ToCharArray();
+            var charLength = charArray.Length;
+            var counter = charLength - 1;
+            var answer = true;
+
+            for (int i = 0; i < charLength; i++)
+            {
+                if (charArray[i] != charArray[counter])
+                    answer = false;
+                counter--;
+            }
+
+
+            return answer;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,23 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
 
+        [InlineData("racecar", true)]
+        [InlineData("three", false)]
+        [InlineData("tacocat", true)]
+        [InlineData("example", false)]
+        [InlineData("aa", true)]
+        [InlineData("a", true)]
+        [InlineData("car", false)]
+
+        public void PalindromeTest(string input, bool expected)
+        {
+            var wordSmith = new WordSmith();
+
+            var actual = wordSmith.IsAPalindrome(input);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
